b09f42e4eb2d5a62f8f0105dcaba6bdbd16c5713
Leonardo Iarossi <leonardo.iarossi@icloud.com>
2026-05-11 15:47:45 +0200

    in pari con esercitazione di 2026_05_07

---
+++ approssimazioneMinimiQuadrati.m
@@ -0,0 +1,15 @@
+function [a] = approssimazioneMinimiQuadrati(x,f)
+arguments (Input) %ottengo due vettori in ingresseo (coppie di punti)
+    x
+    f
+end
+
+arguments (Output) %rendo i coefficenti dell'approssimazione
+    a
+end
+
+A = [length(x), sum(x); sum(x), sum(x.^2)]; %matrice A di forma nota
+b = [sum(f); sum(f.*x)]; %matrice b di forma nota
+a = A\b; %calcolo il vettore a (Aa=b => A*(A^-1)a=b*(A^-1) => Ia=b*(A^-1) => a=b*(A^-1)) A\b è letteralmente la risoluzione algebrica, ma più sicura
+
+end
